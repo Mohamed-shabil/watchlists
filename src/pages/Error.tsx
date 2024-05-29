7745403a333540bbea0213ac7346fd2ccb71d453
Mohamed-shabil <mohamedshabil231@gmail.com>
2024-05-29 20:29:00 +0530

    typo : use Error page props

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -7,7 +7,7 @@ function Error({ text }: { text: string }) {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h2 className="text-xl font-medium">Oops...</h2>
-            <p className="text-sm font-medium">Something Went Wrong</p>
+            <p className="text-sm font-medium">{text}</p>
             <Link to="/">
                 <button className="my-5 p-2 rounded-md flex items-center gap-2 bg-rose-500 text-white text-sm">
                     <Home size={18} /> Go to Home
